358784c0537a71da5576b9bf2b71342075e516a0
Nhlakanipho Mathibela <94120861+NiphoMathibela@users.noreply.github.com>
2023-03-26 19:45:22 +0200

    Commit

--- back-end/server.js
+++ back-end/server.js
@@ -1,43 +1,11 @@
-const express = require("express");
+const express = require('express');
+const mongoose = require('mongoose');
+const port = 3000;
+
 const app = express();
-const port = 3000
-const {MongoClient} = require("mongodb");
 
-async function main (){
-//Connect to cluster
-//Call functions that connect to db
-//Disconnect from cluster
+app.use(express.json());
 
-    const uri = "mongodb+srv://nipho:Excellent@28@workout-tracker.1m7elt8.mongodb.net/?retryWrites=true&w=majority"
-    const client = new MongoClient(uri);
-
-
-    try {
-        await client.connect();
-        await lisDBs(client);
-    } catch (error) {
-        console.error(error);
-    }finally{
-        await client.close();
-    }
-
-}
-
-main().catch(console.error);
-
-async function lisDBs(client){
-    const dbList = await client.db().admin().listDatabases();
-    console.log("Databases: ");
-
-    dbList.databases.forEach(db => {
-        console.log(db.name);
-    });
-}
-
-// app.get('/', (req, res) => {
-//     res.send("Testing...");
-// })
-
-// app.listen(port, () => {
-//     console.log("Server is listening on port " + port)
-// })
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
